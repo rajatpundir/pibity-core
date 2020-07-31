dcf1c25063760d8f90e9b8867591341e63d00a70
Rajat Pundir <rajat.pundir@pibity.com>
2020-07-31 14:05:13 +0530

    Fixed bug

--- src/main/kotlin/com/pibity/erp/commons/VariableUtils.kt
+++ src/main/kotlin/com/pibity/erp/commons/VariableUtils.kt
@@ -133,10 +133,10 @@ fun validateVariableValues(values: JsonObject, type: Type): JsonObject {
             } catch (exception: Exception) {
               throw CustomJsonException("{${key.id.name}: 'Unexpected value for parameter'}")
             }
-            if (jsonArray.size() > key.list!!.max)
-              throw CustomJsonException("{${key.id.name}: 'List cannot contain more than ${key.list!!.max} elements'}")
+            if (key.list!!.max != -1 && jsonArray.size() > key.list!!.max)
+              throw CustomJsonException("{${key.id.name}: 'List cannot contain more than ${key.list!!.max} variables'}")
             if (jsonArray.size() < key.list!!.min)
-              throw CustomJsonException("{${key.id.name}: 'List cannot contain less than ${key.list!!.min} elements'}")
+              throw CustomJsonException("{${key.id.name}: 'List cannot contain less than ${key.list!!.min} variables'}")
             val expectedArray = JsonArray()
             for (ref in jsonArray) {
               if (key.list!!.type.id.superTypeName == "Any") {
