4190a4fe9a99040057fe360bfc5df128e9aa2c19
Rajat Pundir <rajat.pundir@pibity.com>
2020-08-21 15:10:15 +0530

    Add Lisp.kt

---
+++ src/main/kotlin/com/pibity/erp/lisp/Lisp.kt
@@ -0,0 +1,15 @@
+/* 
+ * Copyright (C) 2020 Pibity Infotech Private Limited - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * THIS IS UNPUBLISHED PROPRIETARY CODE OF PIBITY INFOTECH PRIVATE LIMITED
+ * The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
+package com.pibity.erp.lisp
+
+import org.springframework.stereotype.Service
+
+@Service
+class Lisp {
+}
