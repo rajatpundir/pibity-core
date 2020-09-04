4d56b7b5aea696566e83c4765c7c1d5223f9bd7b
Shubham Gupta <shubham.gupta@pibity.com>
2020-09-04 15:10:36 +0530

    keycloak user created and updated using user id not username

--- src/main/kotlin/com/pibity/erp/api/OrganizationController.kt
+++ src/main/kotlin/com/pibity/erp/api/OrganizationController.kt
@@ -8,6 +8,7 @@
 
 package com.pibity.erp.api
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.pibity.erp.commons.getExpectedParams
 import com.pibity.erp.commons.getJsonParams
@@ -15,12 +16,18 @@ import com.pibity.erp.commons.gson
 import com.pibity.erp.commons.logger.Logger
 import com.pibity.erp.services.OrganizationService
 import org.keycloak.KeycloakSecurityContext
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.resource.UserResource
+import org.keycloak.admin.client.resource.UsersResource
+import org.keycloak.representations.idm.CredentialRepresentation
+import org.keycloak.representations.idm.UserRepresentation
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.annotation.security.RolesAllowed
 import javax.servlet.http.HttpServletRequest
+
 
 @CrossOrigin
 @RestController
@@ -51,7 +58,51 @@ class OrganizationController(val organizationService: OrganizationService) {
   fun test(request: HttpServletRequest): ResponseEntity<String> {
     return try {
       println("----------------START HERE------------------------")
-      ResponseEntity(getJWTToken(request), HttpStatus.OK)
+      getJWTToken(request)
+      val serverUrl = "http://localhost:8081/auth"
+      val realm = "inventory"
+      val keycloak = Keycloak.getInstance(
+          serverUrl,
+          "inventory",
+          "testuser",//realm admin
+          "12345678",
+          "test-client",
+          "13823722-e981-41fa-9088-59fe309b83d3")
+
+//      val x=JsonArray()
+//      x.add("zs")
+//      x.add("asd")
+//      x.add("sdfg")
+//
+//      val user  = UserRepresentation()
+//      user.username = "user4"
+//      user.firstName = "test"
+//      user.email = "test4@gmail.com"
+//      user.lastName = "user"
+//      user.isEnabled=true
+//      user.attributes= mapOf("mobile" to listOf("964161616"),"organization" to listOf(x.toString()))
+
+      val realmResource = keycloak.realm(realm)
+//      realmResource.users().create(user)
+
+      val usersResource : UsersResource= realmResource.users()!!
+
+      val passwordCred = CredentialRepresentation()
+      passwordCred.isTemporary = false
+      passwordCred.type = CredentialRepresentation.PASSWORD
+      passwordCred.value = "test"
+
+
+      val userResource: UserResource = usersResource?.get("a9dd7287-428d-44ee-99be-ab2364150dba")
+//      userResource.resetPassword(passwordCred)
+
+
+      val updatedUser = userResource.toRepresentation()
+      updatedUser.email = "A56161616bc@gamil.com"
+      userResource.update(updatedUser)
+
+
+      ResponseEntity("s", HttpStatus.OK)
     } catch (exception: Exception) {
       ResponseEntity("unable to access", HttpStatus.BAD_REQUEST)
     }
